03163c06cf09618aa266e01027fee28e6505f50a
Ulysse <Ulysse@ULYSSEINCORPLT2>
2023-08-27 17:33:20 +0300

    Add project files.

---
+++ ConsoleApp_Module_5_finals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_Module_5_finals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
